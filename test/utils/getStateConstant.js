01486fc19a78db5753189e026feb51e4544d2902
lutangar <johan.dufour@gmail.com>
2017-09-28 18:26:23 +0200

    add getStateConstant test

---
+++ test/utils/getStateConstant.js
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import getStateConstant from "../../lib/utils/getStateConstant";
+
+describe("getStateConstant", () => {
+  it("returns the same exact function when fed with a function", () => {
+    const arg = () => {};
+    expect(getStateConstant(arg)).to.equal(arg);
+  });
+
+  it("returns a function whatever was fed", () => {
+    expect(getStateConstant(undefined)).to.be.a('function');
+    expect(getStateConstant(3)).to.be.a('function');
+    expect(getStateConstant(null)).to.be.a('function');
+    expect(getStateConstant('foo')).to.be.a('function');
+  });
+});
