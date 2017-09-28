80b1f4afd90bf49a79e291c9f192f67ba813f14e
lutangar <johan.dufour@gmail.com>
2017-09-28 14:02:10 +0200

    use ducks conventions for actions constants

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const INITIALIZE = "redux-force/simulation/INITIALIZE";
+export const ADD_NODE = "redux-force/simulation/ADD_NODE";
+export const ADD_NODES = "redux-force/simulation/ADD_NODES";
+export const ADD_LINKS = "redux-force/simulation/ADD_LINKS";
+export const ADD_LINK = "redux-force/simulation/ADD_LINK";
+export const TICK = "redux-force/simulation/TICK";
+export const STOP = "redux-force/simulation/STOP";
+export const RESTART = "redux-force/simulation/RESTART";
