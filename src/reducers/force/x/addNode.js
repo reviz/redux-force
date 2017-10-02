ec89b18978e31d065e99ce0b6fb78bfed96b8b3d
lutangar <johan.dufour@gmail.com>
2017-10-02 10:40:30 +0200

    fix forceX add node

--- src/reducers/force/x/addNode.js
+++ src/reducers/force/x/addNode.js
@@ -4,7 +4,7 @@ import { xz, xStrengths } from "./initialize";
 import { initialState, defaultX, defaultStrength } from "./index";
 import getStateConstant from "../../../utils/getStateConstant";
 
-export default (x = defaultX, strength = defaultStrength) => {
+export default ({ x = defaultX, strength = defaultStrength } = {}) => {
   x = getStateConstant(x);
   strength = getStateConstant(strength);
 
